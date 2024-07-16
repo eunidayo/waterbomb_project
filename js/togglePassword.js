2bc01661780ceb17a6d98aba261bec9e6159cc58
kisung <jjangki2071@gmail.com>
2024-07-16 12:34:23 +0900

    js파일에 주석 추가

--- js/togglePassword.js
+++ js/togglePassword.js
@@ -1,11 +1,13 @@
-document
-  .getElementById("togglePassword")
-  .addEventListener("click", function () {
-    const passwordInput = document.getElementById("password");
-    const type =
-      passwordInput.getAttribute("type") === "password" ? "text" : "password";
-    passwordInput.setAttribute("type", type);
+const togglePassword = document.querySelector(".togglePassword");
+const passwordInput = document.querySelector(".passwordInput");
 
-    // 아이콘 클래스 토글
-    this.classList.toggle("fa-eye-slash");
-  });
+togglePassword.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    // passwordInput 요소의 type 속성이 “password”인지 확인. passwordInput이 비밀번호 입력 필드인지를 확인하는 조건문.
+    passwordInput.type = "text";
+    // passwordInput 요소의 type 속성을 “text”로 변경. 이 줄은 위의 조건문이 참일 때 실행되며, 비밀번호를 일반 텍스트로 표시.
+  } else {
+    passwordInput.type = "password";
+    // passwordInput의 타입을 다시 password로 변경
+  }
+});
